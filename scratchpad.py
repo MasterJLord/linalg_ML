1c4c80df027450d97459ff85ff095013333e7b42
MasterJLord <jayden.cammarata@gmail.com>
2025-10-12 23:21:26 -0400

    bit more messing around

--- scratchpad.py
+++ scratchpad.py
@@ -1,6 +1,15 @@
 import numpy as np
+import pandas as pd
 
 testArray = np.array([["1"], np.array([1])])
 print(testArray)
 testArray = np.random.randint(1, 4, (2, 3, 4))
 print(testArray)
+# testArray = np.array([1, [2, 2], [3, 3]])
+# print(testArray)
+
+testFrame = pd.DataFrame(
+    data = [[1, 2], [4, 5]], # Can work with non-numpy lists
+    columns=("a", "b")
+)
+print(testFrame)
