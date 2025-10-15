4ec06b468b56d7cc339740930c4a35acee04f8a1
MasterJLord <jayden.cammarata@gmail.com>
2025-10-14 21:54:47 -0400

    bit more messing around

--- scratchpad.py
+++ scratchpad.py
@@ -5,9 +5,8 @@ testArray = np.array([["1"], np.array([1])])
 print(testArray)
 testArray = np.random.randint(1, 4, (2, 3, 4))
 print(testArray)
-# testArray += np.random.random((2, 3,4)) - .5
-# testArray = np.array([1, [2, 2], [3, 3]])
-# print(testArray)
+testArray = np.random.random((2, 3,4)) - .5 + testArray
+print(testArray)
 
 testFrame = pd.DataFrame(
     data = [[1, 2], [4, 5]], # Can work with non-numpy lists
@@ -15,7 +14,6 @@ testFrame = pd.DataFrame(
 )
 print(testFrame)
 storage = np.array(testFrame.iloc[0:2])
-print(type(storage))
 testFrame = pd.DataFrame(
     data = storage
 )
