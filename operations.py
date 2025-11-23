c0fbc2e7518da7f3c3607ae4c2a6544725c02a14
MasterJLord <jayden.cammarata@gmail.com>
2025-11-22 23:59:41 -0500

    Wrote code for backpropagation
    I'm tired, it probably doesn't work

--- operations.py
+++ operations.py
@@ -1,23 +1,52 @@
 from fileManipulations import *
 
-def createModel(numInputs : int, neuronsPerLayer : list, activationFunctions : list):
+def CreateModel(numInputs : int, neuronsPerLayer : list, activationFunctions : list):
     pass # might not get around to making this and just stick to the testNet for now
     # Probably will if I want to make a larger neural network than the one I'm currently working with, which I almost certainly will
 
-def forwardPropagation(inputs : np.array, activationFunctions: np.array, weights : np.array, biases : np.array, verbose : bool = False):
+def ForwardPropagation(inputs : np.array, activationFunctions: np.array, weights : np.array, verbose : bool = False):
     if (verbose):
         intermediateSteps = []
     lastStepOutputs = inputs
     for layerNum in range(len(weights)):
-        lastStepOutputs = np.append(1, lastStepOutputs)
         nextStepOutputs = np.matmul(weights[layerNum], lastStepOutputs)
-        nextStepOutputs += biases[layerNum]
         for i in range(len(activationFunctions[layerNum])):
             nextStepOutputs[i] = activationFunctions[layerNum](nextStepOutputs[i])
         if (verbose):
-            intermediateSteps.append(lastStepOutputs)
-        lastStepOutputs = nextStepOutputs
+            intermediateSteps.append(nextStepOutputs)
+        lastStepOutputs = np.append(1, nextStepOutputs)
     if (verbose):
         return intermediateSteps
     else:
-        return lastStepOutputs
+        return lastStepOutputs[1]
+
+
+def BackPropagation(inputs : np.array, label : float, activationFunctions : np.array, weights : np.array, learningRate : float, lossFunction : str = "L2"):
+    allOutputs = ForwardPropagation(inputs, activationFunctions, weights, True)
+    outputLosses = []
+    # Find derivative of loss with respect to output
+    if (lossFunction == "L2"):
+        activationFunc = activationFunctions[len(activationFunctions) - 1][0]
+        activationFuncDerivative = ACTIVATION_FUNCTION_DERIVATIVES[activationFunc]
+        outputMultiple = (activationFunc(allOutputs[len(allOutputs)-1][0])-label) * activationFuncDerivative(allOutputs[len(allOutputs)-1][0])
+        outputLosses.append(outputMultiple * weights[len(weights)-1])
+    else:
+        raise("Invalid loss function")
+    # Find derivative of loss with respect to output
+    for layerIndex in range(len(weights)-1, 0, -1): 
+        # iterates over every layer except for the first one, and finds the losses for the layer that precedes each of them
+        layerOutputLosses = np.zeros(len(weights[layerIndex-1]))
+        for neuronNum in range(len(weights[layerIndex])):
+            activationFuncDerivative = ACTIVATION_FUNCTION_DERIVATIVES[activationFunctions[layerIndex][neuronNum]]
+            lossMultiplier = outputLosses[0][neuronNum] * activationFuncDerivative(allOutputs[layerIndex][neuronNum])
+            layerOutputLosses += lossMultiplier * weights[layerIndex][neuronNum]
+        layerOutputLosses[0] = 0 # TODO : Make sure the way I'm handling biases works
+        outputLosses.insert(0, layerOutputLosses)
+    # Find derivative of loss with respect to weights and update weights accordingly
+    for layerIndex in range(len(weights)):
+        for neuronNum in range(len(weights[layerIndex])):
+            activationFunc = activationFunctions[layerIndex][neuronNum]
+            activationFuncDerivative = ACTIVATION_FUNCTION_DERIVATIVES[activationFunc]
+            lossMultiplier = outputLosses[layerIndex][neuronNum] * activationFuncDerivative(allOutputs[layerIndex][neuronNum]) * learningRate
+            for weight in range(len(weights[layerIndex][neuronNum])):
+                weights[layerIndex][neuronNum][weight] -= activationFunc((inputs if layerIndex == 0 else allOutputs[layerIndex-1])[weight]) * lossMultiplier
